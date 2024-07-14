9c106d361f1e4bbf9dfb23722441675c42c44613
Harshalikale <harshalikale20@gmail.com>
2024-07-14 09:19:59 +0530

    key of loop

---
+++ index1.js
@@ -0,0 +1,13 @@
+const students = [
+    "Harshali",
+    "Roshani",
+    "Neha",
+    "gunjan",
+    "Vedika"
+    
+    ]
+    
+    for(const friend of students)
+    {
+        console.log(`Hello ${friend}`)
+    }
